cb98d677154c346ed0750db9b6037eadb1febe1a
Minoru Osuka <minoru.osuka@gmail.com>
2021-03-10 23:32:38 +0900

    Move to edition 2018

--- meteora-proto/protoc_rust.sh
+++ meteora-proto/protoc_rust.sh
@@ -12,17 +12,18 @@ do
   protoc --proto_path="${PROTO_DIR}" --rust_out=./src/proto --grpc_out=./src/proto --plugin=protoc-gen-grpc=${GRPC_RUST_PLUGIN} ${PROTO_FILES}
 done
 
+MODULE_FILE=./src/proto.rs
 RS_FILES=$(find ./src/proto -name '*.rs' -print0 | xargs -0 -n1 basename | sort | uniq)
-echo "// This file is generated. Do not edit" > ./src/proto/mod.rs
-echo "" >> ./src/proto/mod.rs
+echo "// This file is generated. Do not edit" > ${MODULE_FILE}
+echo "" >> ${MODULE_FILE}
 
-echo "use raft::eraftpb;" >> ./src/proto/mod.rs
+echo "use raft::eraftpb;" >> ${MODULE_FILE}
 
-echo "" >> ./src/proto/mod.rs
+echo "" >> ${MODULE_FILE}
 for RS_FILE in ${RS_FILES}
 do
   MODULE_NAME=$(echo "${RS_FILE}" | awk -F'.' '{print $1}')
-  echo "pub mod ${MODULE_NAME};" >> ./src/proto/mod.rs
+  echo "pub mod ${MODULE_NAME};" >> ${MODULE_FILE}
 done
 
 cargo fmt
